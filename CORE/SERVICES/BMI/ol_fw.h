e439766c10bfb96a66efe04137d8ee4831ada48c
Fugang Duan <fugang.duan@nxp.com>
2018-04-02 15:33:38 +0800

    LEA.NRT_2.0: fix the build error
    Fix the build error for PCI host in LEA.NRT_1.0 version.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- CORE/SERVICES/BMI/ol_fw.h
+++ CORE/SERVICES/BMI/ol_fw.h
@@ -162,11 +162,11 @@ void ol_target_failure(void *instance, A_STATUS status);
 u_int8_t ol_get_number_of_peers_supported(struct ol_softc *scn);
 
 #ifdef REMOVE_PKT_LOG
-static inline void ol_pktlog_init(void *)
+static inline void ol_pktlog_init(void *hif_sc)
 {
 }
 #else
-void ol_pktlog_init(void *);
+void ol_pktlog_init(void *hif_sc);
 #endif
 
 #if defined(HIF_SDIO)
