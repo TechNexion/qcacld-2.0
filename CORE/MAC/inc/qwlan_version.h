45ca65d33eeb59eeba96d2832dde0c662f55dbb8
Anjaneedevi Kapparapu <akappara@codeaurora.org>
2018-11-26 12:13:51 +0530

    Release  Version 4.5.25.1
    Release  Version 4.5.25.1

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,11 +40,11 @@ BRIEF DESCRIPTION:
 
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            5
-#define QWLAN_VERSION_PATCH            24
+#define QWLAN_VERSION_PATCH            25
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            2
+#define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "4.5.24.2"
+#define QWLAN_VERSIONSTR               "4.5.25.1"
 
 
 #define AR6320_REV1_VERSION             0x5000000
