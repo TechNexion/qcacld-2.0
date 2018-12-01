0b9250db091a63330a1b1e36d40fd9105c17ca36
Anjaneedevi Kapparapu <akappara@codeaurora.org>
2018-12-01 13:09:18 +0530

    Release Version 4.5.24.3
    Release Version 4.5.24.3
    
    Change-Id: I4c463d17cae573652f1ea8f23027ecb086b04a5d

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            5
 #define QWLAN_VERSION_PATCH            24
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            2
+#define QWLAN_VERSION_BUILD            3
 
-#define QWLAN_VERSIONSTR               "4.5.24.2"
+#define QWLAN_VERSIONSTR               "4.5.24.3"
 
 
 #define AR6320_REV1_VERSION             0x5000000
