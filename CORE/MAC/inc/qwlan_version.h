df6759b40d59c25da794fbd4f42ff7428848fa65
Brandon Huang <yanghuan@codeaurora.org>
2020-04-08 16:54:13 +0800

    Release Version 4.5.25.40
    Release Version 4.5.25.40
    
    Change-Id: Iddb76a030cb19cada01479bdb54aa4dfeb76820f

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            5
 #define QWLAN_VERSION_PATCH            25
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            39
+#define QWLAN_VERSION_BUILD            40
 
-#define QWLAN_VERSIONSTR               "4.5.25.39"
+#define QWLAN_VERSIONSTR               "4.5.25.40"
 
 
 #define AR6320_REV1_VERSION             0x5000000
