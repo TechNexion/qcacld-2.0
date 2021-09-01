9f0a71561c8084fd6b75752895aa6b8db8503e7b
lihual <lihual@codeaurora.org>
2021-09-01 14:07:28 +0800

    Release 4.5.25.54
    Release 4.5.25.54
    Change-Id: If26f387192328a3ef07a1fc57561b2b506eb9f60

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            5
 #define QWLAN_VERSION_PATCH            25
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            53
+#define QWLAN_VERSION_BUILD            54
 
-#define QWLAN_VERSIONSTR               "4.5.25.53"
+#define QWLAN_VERSIONSTR               "4.5.25.54"
 
 
 #define AR6320_REV1_VERSION             0x5000000
